753d076ac918a847164e15b21c4161789bafd62a
inha1234 <wind150804@gmail.com>
2025-01-09 10:55:21 +0900

    스프링 시큐리티 적용

---
+++ backend/src/main/java/com/onion/backend/jwt/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.onion.backend.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private String secretKey = "AJKhs4YkR9Kl3eV9yOmcK17pZp6x8aOp12D9sZlxRU4=";
+
+    private long expriationTime = 3600000;
+
+    // JWT 토큰 생성
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expriationTime);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
--- backend/src/main/java/com/onion/backend/service/UserService.java
+++ backend/src/main/java/com/onion/backend/service/UserService.java
@@ -1,29 +1,37 @@
 package com.onion.backend.service;
 
+import com.onion.backend.dto.SignUpUser;
 import com.onion.backend.entity.User;
 import com.onion.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public User createUser(String username, String password, String email) {
+    public User createUser(SignUpUser signUpUser) {
         User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
+        user.setUsername(signUpUser.getUsername());
+        user.setPassword(passwordEncoder.encode(signUpUser.getPassword()));
+        user.setEmail(signUpUser.getEmail());
         return userRepository.save(user);
     }
 
     public void deleteUser(Long userId){
         userRepository.deleteById(userId);
     }
+
+    public List<User> getUsers(){
+    return userRepository.findAll();}
 }
